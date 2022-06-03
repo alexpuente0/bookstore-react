7ca5ec6b92d39c502ef353517b87a20451ef4877
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-06-03 14:24:48 -0500

    Setup App and Index files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BookContainer from './components/bookContainer';
+import './index.css';
+import Navbar from './components/navBar';
+import Categories from './pages/categories';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookContainer />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
