5ab556d7343a0a8c517cc599363cee2b1c54362d
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-06-07 10:01:16 -0500

    Made Add and Remove buttons Functional

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,31 +1,14 @@
+import { useSelector } from 'react-redux';
 import Book from './book';
 
 const BookList = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'John Dies at the End',
-      author: 'David Wong',
-    },
-    {
-      id: 2,
-      title: 'IT',
-      author: 'Stephen King',
-    },
-    {
-      id: 3,
-      title: 'Harry Potter and the Chamber of Secrets',
-      author: 'J.K. Rowling',
-    },
-  ];
+  const books = useSelector((state) => state.books);
   return (
-    <main>
-      <ul>
-        {books.map(({ id, title, author }) => (
-          <Book key={id} title={title} author={author} />
-        ))}
-      </ul>
-    </main>
+    <div className="book-list">
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </div>
   );
 };
 
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,24 +3,28 @@ import { v4 as uuidv4 } from 'uuid';
 const ADD_BOOK = 'bookstore-react/book/ADD_BOOK';
 const DELETE_BOOK = 'bookstore-react/book/DELETE_BOOK';
 
-const initialState = [];
+const initialState = [
+  { title: 'The Da Vinci Code', author: 'Dan Brown', id: uuidv4() },
+  { title: 'Catcher in the Rye', author: 'J.D. Salinger', id: uuidv4() },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', id: uuidv4() },
+];
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.data];
     case DELETE_BOOK:
-      return state.filter((book) => book.id !== action.data.id);
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }
 }
 
-export function addBook(title = '', author = '') {
+export function addBook(book) {
   const id = uuidv4();
   return {
     type: ADD_BOOK,
-    data: { title, author, id },
+    data: { ...book, id },
   };
 }
 
