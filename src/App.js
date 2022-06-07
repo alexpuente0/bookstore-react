a679c8fbfdf1d33628af33d2c26f7114dfd5098c
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-06-07 17:39:34 -0500

    Added Remove and add book functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import BookContainer from './components/bookContainer';
 import './index.css';
 import Navbar from './components/navBar';
 import Categories from './pages/categories';
 
+import store from './redux/configureStore';
+
 const App = () => (
-  <>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <Navbar />
       <Routes>
         <Route path="/" element={<BookContainer />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
-    </BrowserRouter>
-  </>
+    </Provider>
+  </BrowserRouter>
 );
 
 export default App;
